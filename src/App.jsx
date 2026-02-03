dc4455c09d9b25d7353f5b81fced94e2d7fb8571
Abanob <aba.said2002@gmail.com>
2026-02-03 14:30:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// bootstrap si fa facendo npm install bootstrap e poi mettere import come vedi
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Row, Col } from "react-bootstrap"
+
+function App() {
+  return <></>
+}
+
+export default App
